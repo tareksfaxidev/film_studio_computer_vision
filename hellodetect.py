de9a14dbe28d28627236f1200546b89f59fa74cc
tarek Gift <tareksfaxi@gmail.com>
2022-04-23 20:44:18 +0000

    ajouter travail cli

--- hellodetect.py
+++ hellodetect.py
@@ -14,4 +14,16 @@ def detect(bucket, name):
         }
     )
     return response
-print(detect(bucket="projetfilm", name="52128.jpg"))
+
+
+@click.command(help="This is a computer vision application.")
+@click.option("--name", prompt="I need the image filename", help="Need Image filename")
+@click.option("--bucket", prompt="I need the bucketname", help="bucket")
+@click.option("--color", prompt="I need your color", help="This is your color")
+def cli(bucket, name, color):
+    result = detect(bucket, name)
+    click.echo(click.style(f"These are your labels! {result}", fg=color))
+
+
+if __name__ == "__main__":
+    cli()
